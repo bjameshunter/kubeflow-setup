ae5365f96d83886807495ca84d3a10be2b5bbdf0
Ben Hunter <ben.hunter@levvel.io>
2021-10-09 16:49:13 -0500

    added script to configure local after creating on GCP shell

--- config-local.sh
+++ config-local.sh
@@ -11,13 +11,14 @@ gcloud config set compute/zone ${ZONE}
 ## MGMT
 # # Add the kustomize package to your $PATH env variable
 # sudo mv ./kustomize /usr/local/bin/kustomize
-
+export KF_NAME=$(gcloud container clusters list | grep kubeflow | awk '{ print $1 }')
 gcloud container clusters get-credentials "${MGMT_NAME}" --zone "${ZONE}" --project "${PROJECT_ID}"
 kubectl config delete-context $MGMT_NAME
 kubectl config rename-context $(kubectl config current-context) $MGMT_NAME
 kubectl config delete-context $KF_NAME
-# gcloud container clusters get-credentials "${KF_NAME}" --zone "${ZONE}" --project "${PROJECT_ID}"
-# kubectl config rename-context $(kubectl config current-context) $KF_PROJECT
+gcloud container clusters get-credentials "${KF_NAME}" --zone "${ZONE}" --project "${PROJECT_ID}"
+kubectl config rename-context $(kubectl config current-context) kubeflow
 
+export KF_UI=$(kubectl --context kubeflow -n istio-system get ingress envoy-ingress -o=jsonpath='{.spec.rules[0].host}')
 
 ## for later
